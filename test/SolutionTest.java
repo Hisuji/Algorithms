41a7ff53bf5b14a75b12e40ba4f6eb96a3a09b7f
DESKTOP-ROA2184\sujik <sujikim.dev@gmail.com>
2021-08-08 11:35:48 +0900

    평균구하기 test - test annotation 추가

--- test/SolutionTest.java
+++ test/SolutionTest.java
@@ -1,22 +1,27 @@
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 
 public class SolutionTest extends TestCase {
     Solution solution;
+
     int[] trulyArr = {1, 2, 3, 4};
     int[] falsyLengthArr = {};
     int[] falsyRangeArr = {1, 23000, 5};
 
+    @Test
     public void testSolution() {
         solution = new Solution();
         assertEquals(solution.solution(trulyArr), 2.5);
-
     }
 
+    @Test
     public void testCheckLength() {
         solution = new Solution();
-        assertEquals(solution.checkLength(falsyLengthArr), true);
+        assertEquals(solution.checkLength(falsyLengthArr), false);
     }
 
+    @Test
     public void testcheckRange() {
         solution = new Solution();
         assertEquals(solution.checkRange(falsyRangeArr), false);
