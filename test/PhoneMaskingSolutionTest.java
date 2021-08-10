0c9f158121e4f4b4812d1021afd3b3221ffbb6bf
DESKTOP-ROA2184\sujik <sujikim.dev@gmail.com>
2021-08-10 21:59:22 +0900

    핸드폰번호가리기 알고리즘 TEST 중복 코드 수정

--- test/PhoneMaskingSolutionTest.java
+++ test/PhoneMaskingSolutionTest.java
@@ -4,22 +4,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneMaskingSolutionTest {
+    PhoneMaskingSolution phoneMaskingSolution = new PhoneMaskingSolution();
 
     @Test
     void solution() throws Exception {
-        PhoneMaskingSolution phoneMaskingSolution = new PhoneMaskingSolution();
         assertEquals(phoneMaskingSolution.solution("01012345678"), "*******5678");
     }
 
     @Test
     void checkPhoneNumberLength() {
-        PhoneMaskingSolution phoneMaskingSolution = new PhoneMaskingSolution();
         assertEquals(phoneMaskingSolution.checkPhoneNumberLength("01012345678"), true);
     }
 
     @Test
     void replacePhoneNumber() {
-        PhoneMaskingSolution phoneMaskingSolution = new PhoneMaskingSolution();
         assertEquals(phoneMaskingSolution.replacePhoneNumber("01012345678"), "*******5678");
     }
 }
