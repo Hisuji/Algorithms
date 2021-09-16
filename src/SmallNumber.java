e66d40856b78dd731049f04b365f75a1a894217d
Hisuji <sujikim.dev@gmail.com>
2021-09-16 23:57:44 +0900

    배열 선언 및 값 초기화 방법 수정

--- src/SmallNumber.java
+++ src/SmallNumber.java
@@ -5,9 +5,7 @@ public class SmallNumber {
     public int[] solution(int[] arr) {
         int[] newArr = Arrays.stream(arr).filter(num -> num != Arrays.stream(arr).min().getAsInt()).toArray();
         if (newArr.length == 0) {
-            newArr = new int[1];
-            newArr[0] = -1;
-            return newArr;
+            return new int[]{-1};
         }
         return newArr;
     }
